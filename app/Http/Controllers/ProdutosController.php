609e1e4037cc42cadb10b247fa12f034d8aad460
Edson Kennis <edson.k.santos@outlook.com>
2021-11-10 19:39:10 -0300

    Rota modificada

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produtos;
+use Illuminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+   
+    public function index()
+    {
+        //
+    }
+
+   
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show(Produtos $produtos)
+    {
+        //
+    }
+
+   
+    public function update(Request $request, Produtos $produtos)
+    {
+        //
+    }
+
+    
+    public function destroy(Produtos $produtos)
+    {
+        //
+    }
+}
