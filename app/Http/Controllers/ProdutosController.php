98fa8ef8d397676a77cc8e2751f15d015be67b49
Edson Kennis <edson.k.santos@outlook.com>
2021-11-11 10:33:35 -0300

    CRUD finalizado

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -10,32 +10,36 @@ class ProdutosController extends Controller
    
     public function index()
     {
-        //
+        $produtos = Produtos::all();
+        return $produtos;
     }
 
    
     public function store(Request $request)
     {
-       $marca =  Produtos::create($request->all());
-        return $marca;
+       $produto =  Produtos::create($request->all());
+        return $produto;
         
     }
 
     
-    public function show(Produtos $produtos)
+    public function show($id)
     {
-        //
+        
+        return Produtos::findOrFail($id);
     }
 
    
-    public function update(Request $request, Produtos $produtos)
+    public function update(Request $request, $id)
     {
-        //
+       $produto = Produtos::findOrFail($id);
+       $produto->update($request->all());
     }
 
     
-    public function destroy(Produtos $produtos)
+    public function destroy($id)
     {
-        //
+        $produto = Produtos::findOrFail($id);
+        $produto->delete();
     }
 }
