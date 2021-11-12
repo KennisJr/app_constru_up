90bb5fd172fbf64758965cfac2b9a1028a483f3a
Edson Kennis <edson.k.santos@outlook.com>
2021-11-12 00:09:08 -0300

    cadastrar front+back finalizado!

--- public/js/app.js
+++ public/js/app.js
@@ -2093,8 +2093,13 @@ __webpack_require__.r(__webpack_exports__);
 /*!************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/Index.vue?vue&type=script&lang=js& ***!
   \************************************************************************************************************************************************************************************************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
 //
 //
 //
@@ -2154,8 +2159,38 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  data: function data() {
+    return {
+      urlBase: 'http://localhost:8000/api/produtos',
+      nomeProduto: '',
+      descricaoProduto: '',
+      marcaProduto: '',
+      valorProduto: ''
+    };
+  },
+  methods: {
+    salvar: function salvar() {
+      var formData = new FormData();
+      formData.append('nome', this.nomeProduto);
+      formData.append('descricao', this.descricaoProduto);
+      formData.append('marca', this.marcaProduto);
+      formData.append('valor', this.valorProduto);
+      var config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Accept': 'application/json'
+        }
+      };
+      axios.post(this.urlBase, formData, config).then(function (response) {
+        alert("Produto cadastrado com sucesso!");
+        console.log(response);
+      })["catch"](function (erros) {
+        console.log(erros);
+      });
+    }
+  }
+});
 
 /***/ }),
 
@@ -37681,9 +37716,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _Index_vue_vue_type_template_id_bb962f12___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Index.vue?vue&type=template&id=bb962f12& */ "./resources/js/components/Index.vue?vue&type=template&id=bb962f12&");
 /* harmony import */ var _Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Index.vue?vue&type=script&lang=js& */ "./resources/js/components/Index.vue?vue&type=script&lang=js&");
-/* harmony reexport (unknown) */ var __WEBPACK_REEXPORT_OBJECT__ = {};
-/* harmony reexport (unknown) */ for(const __WEBPACK_IMPORT_KEY__ in _Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(__WEBPACK_IMPORT_KEY__ !== "default") __WEBPACK_REEXPORT_OBJECT__[__WEBPACK_IMPORT_KEY__] = () => _Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[__WEBPACK_IMPORT_KEY__]
-/* harmony reexport (unknown) */ __webpack_require__.d(__webpack_exports__, __WEBPACK_REEXPORT_OBJECT__);
 /* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
@@ -37780,11 +37812,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Index.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/Index.vue?vue&type=script&lang=js&");
-/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__);
-/* harmony reexport (unknown) */ var __WEBPACK_REEXPORT_OBJECT__ = {};
-/* harmony reexport (unknown) */ for(const __WEBPACK_IMPORT_KEY__ in _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__) if(__WEBPACK_IMPORT_KEY__ !== "default") __WEBPACK_REEXPORT_OBJECT__[__WEBPACK_IMPORT_KEY__] = () => _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__[__WEBPACK_IMPORT_KEY__]
-/* harmony reexport (unknown) */ __webpack_require__.d(__webpack_exports__, __WEBPACK_REEXPORT_OBJECT__);
- /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0___default())); 
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
@@ -37923,135 +37951,197 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
-}
-var staticRenderFns = [
-  function () {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { attrs: { calss: "container" } }, [
-      _c("div", { staticClass: "row justify-content-center" }, [
-        _c("div", { staticClass: "col-md-8" }, [
-          _c("div", { staticClass: "card" }, [
-            _c("div", { staticClass: "card-header" }, [_vm._v("Cadastro")]),
-            _vm._v(" "),
-            _c("div", { staticClass: "card-body" }, [
-              _c("form", { attrs: { method: "POST", action: "" } }, [
-                _c("div", { staticClass: "form-group row" }, [
-                  _c(
-                    "label",
-                    {
-                      staticClass: "col-md-4 col-form-label text-md-right",
-                      attrs: { for: "name" },
-                    },
-                    [_vm._v("Nome:")]
-                  ),
-                  _vm._v(" "),
-                  _c("div", { staticClass: "col-md-6" }, [
-                    _c("input", {
-                      staticClass: "form-control",
-                      attrs: {
-                        id: "name",
-                        type: "text",
-                        name: "name",
-                        value: "",
-                      },
-                    }),
-                  ]),
-                ]),
+  return _c("div", { attrs: { calss: "container" } }, [
+    _c("div", { staticClass: "row justify-content-center" }, [
+      _c("div", { staticClass: "col-md-8" }, [
+        _c("div", { staticClass: "card" }, [
+          _c("div", { staticClass: "card-header" }, [_vm._v("Cadastro")]),
+          _vm._v(" "),
+          _c("div", { staticClass: "card-body" }, [
+            _c("form", { attrs: { method: "POST", action: "" } }, [
+              _c("div", { staticClass: "form-group row" }, [
+                _c(
+                  "label",
+                  {
+                    staticClass: "col-md-4 col-form-label text-md-right",
+                    attrs: { for: "name" },
+                  },
+                  [_vm._v("Nome:")]
+                ),
                 _vm._v(" "),
-                _c("div", { staticClass: "form-group row" }, [
-                  _c(
-                    "label",
-                    {
-                      staticClass: "col-md-4 col-form-label text-md-right",
-                      attrs: { for: "descricao" },
-                    },
-                    [_vm._v("Descrição:")]
-                  ),
-                  _vm._v(" "),
-                  _c("div", { staticClass: "col-md-6" }, [
-                    _c("input", {
-                      staticClass: "form-control",
-                      attrs: {
-                        id: "descricao",
-                        type: "text",
-                        name: "descricao_produto",
-                        value: "",
-                      },
-                    }),
-                  ]),
-                ]),
-                _vm._v(" "),
-                _c("div", { staticClass: "form-group row" }, [
-                  _c(
-                    "label",
-                    {
-                      staticClass: "col-md-4 col-form-label text-md-right",
-                      attrs: { for: "marca" },
-                    },
-                    [_vm._v("Marca:")]
-                  ),
-                  _vm._v(" "),
-                  _c("div", { staticClass: "col-md-6" }, [
-                    _c("input", {
-                      staticClass: "form-control",
-                      attrs: {
-                        id: "marca",
-                        type: "text",
-                        name: "marca_produto",
-                      },
-                    }),
-                  ]),
-                ]),
-                _vm._v(" "),
-                _c("div", { staticClass: "form-group row" }, [
-                  _c(
-                    "label",
-                    {
-                      staticClass: "col-md-4 col-form-label text-md-right",
-                      attrs: { for: "valor" },
-                    },
-                    [_vm._v("Valor:")]
-                  ),
-                  _vm._v(" "),
-                  _c("div", { staticClass: "col-md-6" }, [
-                    _c("input", {
-                      staticClass: "form-control",
-                      attrs: {
-                        id: "valor",
-                        type: "text",
-                        name: "valor_produto",
-                      },
-                    }),
-                  ]),
-                ]),
-                _vm._v(" "),
-                _c("div", { staticClass: "form-group row mb-0" }, [
-                  _c("div", { staticClass: "col-md-6 offset-md-4" }, [
-                    _c(
-                      "button",
+                _c("div", { staticClass: "col-md-6" }, [
+                  _c("input", {
+                    directives: [
                       {
-                        staticClass: "btn btn-primary",
-                        attrs: { type: "submit" },
+                        name: "model",
+                        rawName: "v-model",
+                        value: _vm.nomeProduto,
+                        expression: "nomeProduto",
                       },
-                      [
-                        _vm._v(
-                          "\n                                   Cadastrar\n                                "
-                        ),
-                      ]
-                    ),
-                  ]),
+                    ],
+                    staticClass: "form-control",
+                    attrs: {
+                      id: "name",
+                      type: "text",
+                      name: "name",
+                      value: "",
+                    },
+                    domProps: { value: _vm.nomeProduto },
+                    on: {
+                      input: function ($event) {
+                        if ($event.target.composing) {
+                          return
+                        }
+                        _vm.nomeProduto = $event.target.value
+                      },
+                    },
+                  }),
+                ]),
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: "form-group row" }, [
+                _c(
+                  "label",
+                  {
+                    staticClass: "col-md-4 col-form-label text-md-right",
+                    attrs: { for: "descricao" },
+                  },
+                  [_vm._v("Descrição:")]
+                ),
+                _vm._v(
+                  "\n                            " +
+                    _vm._s(_vm.descricaoProduto) +
+                    "\n                            "
+                ),
+                _c("div", { staticClass: "col-md-6" }, [
+                  _c("input", {
+                    directives: [
+                      {
+                        name: "model",
+                        rawName: "v-model",
+                        value: _vm.descricaoProduto,
+                        expression: "descricaoProduto",
+                      },
+                    ],
+                    staticClass: "form-control",
+                    attrs: {
+                      id: "descricao",
+                      type: "text",
+                      name: "descricao_produto",
+                      value: "",
+                    },
+                    domProps: { value: _vm.descricaoProduto },
+                    on: {
+                      input: function ($event) {
+                        if ($event.target.composing) {
+                          return
+                        }
+                        _vm.descricaoProduto = $event.target.value
+                      },
+                    },
+                  }),
+                ]),
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: "form-group row" }, [
+                _c(
+                  "label",
+                  {
+                    staticClass: "col-md-4 col-form-label text-md-right",
+                    attrs: { for: "marca" },
+                  },
+                  [_vm._v("Marca:")]
+                ),
+                _vm._v(" "),
+                _c("div", { staticClass: "col-md-6" }, [
+                  _c("input", {
+                    directives: [
+                      {
+                        name: "model",
+                        rawName: "v-model",
+                        value: _vm.marcaProduto,
+                        expression: "marcaProduto",
+                      },
+                    ],
+                    staticClass: "form-control",
+                    attrs: { id: "marca", type: "text", name: "marca_produto" },
+                    domProps: { value: _vm.marcaProduto },
+                    on: {
+                      input: function ($event) {
+                        if ($event.target.composing) {
+                          return
+                        }
+                        _vm.marcaProduto = $event.target.value
+                      },
+                    },
+                  }),
+                ]),
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: "form-group row" }, [
+                _c(
+                  "label",
+                  {
+                    staticClass: "col-md-4 col-form-label text-md-right",
+                    attrs: { for: "valor" },
+                  },
+                  [_vm._v("Valor:")]
+                ),
+                _vm._v(" "),
+                _c("div", { staticClass: "col-md-6" }, [
+                  _c("input", {
+                    directives: [
+                      {
+                        name: "model",
+                        rawName: "v-model",
+                        value: _vm.valorProduto,
+                        expression: "valorProduto",
+                      },
+                    ],
+                    staticClass: "form-control",
+                    attrs: { id: "valor", type: "text", name: "valor_produto" },
+                    domProps: { value: _vm.valorProduto },
+                    on: {
+                      input: function ($event) {
+                        if ($event.target.composing) {
+                          return
+                        }
+                        _vm.valorProduto = $event.target.value
+                      },
+                    },
+                  }),
+                ]),
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: "form-group row mb-0" }, [
+                _c("div", { staticClass: "col-md-6 offset-md-4" }, [
+                  _c(
+                    "button",
+                    {
+                      staticClass: "btn btn-primary",
+                      attrs: { type: "button" },
+                      on: {
+                        click: function ($event) {
+                          return _vm.salvar()
+                        },
+                      },
+                    },
+                    [
+                      _vm._v(
+                        "\n                                   Cadastrar\n                                "
+                      ),
+                    ]
+                  ),
                 ]),
               ]),
             ]),
           ]),
         ]),
       ]),
-    ])
-  },
-]
+    ]),
+  ])
+}
+var staticRenderFns = []
 render._withStripped = true
 
 
--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -12,18 +12,15 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">Nome:</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="">
-
+                                <input id="name" type="text" class="form-control" name="name" value="" v-model="nomeProduto">
             
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="descricao" class="col-md-4 col-form-label text-md-right">Descrição:</label>
-
                             <div class="col-md-6">
-                                <input id="descricao" type="text" class="form-control" name="descricao_produto" value="">
-
+                                <input id="descricao" type="text" class="form-control" name="descricao_produto" value="" v-model="descricaoProduto">
                             </div>
                         </div>
 
@@ -31,8 +28,7 @@
                             <label for="marca" class="col-md-4 col-form-label text-md-right">Marca:</label>
 
                             <div class="col-md-6">
-                                <input id="marca" type="text" class="form-control" name="marca_produto">
-
+                                <input id="marca" type="text" class="form-control" name="marca_produto" v-model="marcaProduto">
                             </div>
                         </div>
 
@@ -40,13 +36,13 @@
                             <label for="valor" class="col-md-4 col-form-label text-md-right">Valor:</label>
 
                             <div class="col-md-6">
-                                <input id="valor" type="text" class="form-control" name="valor_produto" >
+                                <input id="valor" type="text" class="form-control" name="valor_produto" v-model="valorProduto" >
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="button" class="btn btn-primary" @click="salvar()">
                                    Cadastrar
                                 </button>
                             </div>
@@ -60,5 +56,39 @@
 </template>
 
 <script>
-  
+  export default {
+      data() {
+          return {
+              urlBase: 'http://localhost:8000/api/produtos',
+              nomeProduto: '',
+              descricaoProduto: '',
+              marcaProduto: '',
+              valorProduto: '',
+          };
+      },
+      methods: {
+          salvar(){
+             let formData =new FormData();
+              formData.append('nome',this.nomeProduto)
+              formData.append('descricao',this.descricaoProduto)
+              formData.append('marca',this.marcaProduto)
+              formData.append('valor',this.valorProduto)
+
+              let config = {
+                  headers: {
+                    'Content-Type':'multipart/form-data',
+                    'Accept': 'application/json'
+                  }
+              }
+             axios.post(this.urlBase,formData, config)
+                .then(response =>{
+                    alert("Produto cadastrado com sucesso!");
+                    console.log(response)
+                })
+                .catch(erros => {
+                    console.log(erros)
+                })
+          }
+      }
+  }
 </script>
